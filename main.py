76353b34adb0be97dab1a1a1df2bf3bd7768b1ed
ShawonAshraf <shawon13@live.com>
2017-07-13 19:47:03 +0600

    Added stats count

--- main.py
+++ main.py
@@ -25,7 +25,20 @@ recWriter.writeRecords(text_file=mailingListFile, records=mail_list)
 # writing mailing lists here
 teamNames = ["Corporate", "Operations", "Publications", "Promotions", "Logistics"]
 
+# for stats
+stats = {}
+total = 0
+
 for teamName in teamNames:
     teamMailingFile = "Mailing/{}.txt".format(teamName)
     mail_list = extractor.getEmailByTeam(teamName=teamName)
     recWriter.writeRecords(text_file=teamMailingFile, records=mail_list)
+
+    total = total + len(mail_list)
+    stats[teamName] = len(mail_list)
+
+print("====================STATS=====================")
+print("===================Summer17===================")
+print("\n\nTotal recruited : {}\n".format(total))
+for teamName in teamNames:
+    print("{} : {}".format(teamName, stats[teamName]))
