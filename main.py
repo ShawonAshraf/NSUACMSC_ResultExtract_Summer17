ba0fd0022b36fd3c60f25c1faa695db21b262bd5
ShawonAshraf <shawon13@live.com>
2017-07-13 21:37:48 +0600

    Fixed stats

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from Extractor import Extractor
 from InfoToTextFile import RecordEntry
-from Stats import Stats
 
 excelFile = "FinalResult.xlsx"
-mailingListFile = "Mailing/MailingList.txt"
+mailingListFile = "Mailing/MailingList.csv"
 
 # list of emails, to be written to a text file
 mail_list = []
@@ -21,11 +20,23 @@ mail_list = extractor.extractAllEmailAddress()
 # write to file
 recWriter.writeRecords(text_file=mailingListFile, records=mail_list)
 
-
 # get team records by name and write mail lists or full info
 # writing mailing lists here
 teamNames = ["Corporate", "Operations", "Publications", "Promotions", "Logistics"]
+stats = {}
 
-# for stats
-stats = Stats()
-stats.getAllStats(workbook=excelFile)
+for teamName in teamNames:
+    recordsFile = "Mailing/{}.csv".format(teamName)
+    records = extractor.extractRecordByTeam(teamName=teamName)
+    stats[teamName] = len(records)
+
+    recWriter.writeRecords(text_file=recordsFile, records=records)
+
+print(stats)
+total = 0
+print("===================Stats :::::::::::::: Summer 17 ===================")
+for t in teamNames:
+    print("\t\t{} : {}".format(t, stats[t]))
+    total = total + stats[t]
+
+print("\t\tTotal Recruited : {}".format(total))
